2373939b23975ed1f8517e50b88fa10ce88664f2
dingin <dingin917@gmail.com>
2019-03-09 14:58:10 +0800

    execute async loop sync

--- server/upload_course.js
+++ server/upload_course.js
@@ -53,37 +53,40 @@ let readCSV = function CSVToArray(req, res, next){
         }
         else {
 
-          for (let i=1; i<fileRows.length; i++) {
-            let aRow = fileRows[i];
-            if(aRow[0].trim()!=''){
-              // get the original group name of the last row to compare with the current one 
-              let last_grpname = last_row[4];
-              if(last_grpname.indexOf('-')>0){
-                last_grpname = last_grpname.slice(0, last_row[4].indexOf('-'));
-              }
-              if(aRow[2] === last_row[2] && aRow[3] === last_row[3] && aRow[4] === last_grpname){
-                // special case: same course code, type & group name 
-                if (index === 0){
-                  // no previous record for the same course code, type & group name combination
-                  last_row[4] =  last_row[4] + '-' + String.fromCharCode(65);
+          const forLoop = async () => {
+            for (let i=1; i<fileRows.length; i++) {
+              let aRow = fileRows[i];
+              if(aRow[0].trim()!=''){
+                // get the original group name of the last row to compare with the current one 
+                let last_grpname = last_row[4];
+                if(last_grpname.indexOf('-')>0){
+                  last_grpname = last_grpname.slice(0, last_row[4].indexOf('-'));
                 }
-                updateCourseToDB(last_row);
-                index++;
-                last_row = aRow.slice(0);
-                last_row[4] = aRow[4] + '-' + String.fromCharCode(65 + index);
-                console.log('\n index: ' + index + '\n name: ' + last_row[4] + '\n aRow: ' + aRow);
-              } else {
-                // general case: same course code & type but diff group || diff course code & type
-                updateCourseToDB(last_row);
-                index = 0;
-                last_row = aRow.slice(0);
-                console.log('\n index: ' + index + '\n name: ' + last_row[4] + '\n aRow: ' + aRow);
+                if(aRow[2] === last_row[2] && aRow[3] === last_row[3] && aRow[4] === last_grpname){
+                  // special case: same course code, type & group name 
+                  if (index === 0){
+                    // no previous record for the same course code, type & group name combination
+                    last_row[4] =  last_row[4] + '-' + String.fromCharCode(65);
+                  }
+                  await updateCourseToDB(last_row);
+                  index++;
+                  last_row = aRow.slice(0);
+                  last_row[4] = aRow[4] + '-' + String.fromCharCode(65 + index);
+                  console.log('\n index: ' + index + '\n name: ' + last_row[4] + '\n aRow: ' + aRow);
+                } else {
+                  // general case: same course code & type but diff group || diff course code & type
+                  await updateCourseToDB(last_row);
+                  index = 0;
+                  last_row = aRow.slice(0);
+                  console.log('\n index: ' + index + '\n name: ' + last_row[4] + '\n aRow: ' + aRow);
+                }
               }
             }
+            // upload the last course to database 
+            await updateCourseToDB(last_row);
           }
-    
-          // upload the last course to database 
-          updateCourseToDB(last_row);
+
+          forLoop();
 
           let html = '';
           html += '<p> The course csv file has been successfully uploaded </p>';
@@ -94,7 +97,56 @@ let readCSV = function CSVToArray(req, res, next){
 
       }).catch(next);
 
-      function updateCourseToDB(arow){
+      const process_course = (arow, venue, group, teaching_weeks) => {
+        return Course.findOneAndUpdate({'acad_yr':acad_yr, 'sem':sem, 'category':category, 'code':arow[2], 'type':arow[3]}, 
+        {
+          $push: {
+            'schedule': {
+              'group': group,
+              'teaching_weeks':  teaching_weeks,
+              'day': arow[5],
+              'start_time': arow[6],
+              'end_time': arow[7],
+              'venue': venue,
+              'unscheduled_weeks': teaching_weeks
+            }
+          }
+        }, { new: true, upsert: true });
+      }
+
+      const func_course = (arow, venue, group, teaching_weeks) => {
+        // Then
+        process_course(arow, venue, group, teaching_weeks).then(result => {          
+          console.log(JSON.stringify(result));
+        });
+      }
+
+      const process_venue = (arow, venue, group, teaching_weeks) => {
+        // update venue utilization db 
+        return VenueUtil.findOneAndUpdate({'venue': venue, 'acad_yr': acad_yr, 'sem': sem}, 
+          {
+            $push: {
+              'scheduled_time': {
+                'course': arow[2],
+                'courseType': arow[3],
+                'group': group,
+                'day': arow[5],
+                'start_time': arow[6],
+                'end_time': arow[7],
+                'week': teaching_weeks,
+              }
+            }
+          }, { new: true, upsert: true});
+      }
+
+      const func_venue = (arow, venue, group, teaching_weeks) => {
+        // Then
+        process_venue(arow, venue, group, teaching_weeks).then(updated_venue => {
+          console.log(updated_venue);
+        });
+      }
+
+      const updateCourseToDB = async (arow) => {
         // re-format venue, merge XX-XX-XX. with XX-XX-XX 
         let venue = arow[8];
         if(venue.slice(-1) === '.'){
@@ -115,68 +167,10 @@ let readCSV = function CSVToArray(req, res, next){
             teaching_weeks.push(j-8);
           }
         }
-        Course.findOneAndUpdate({'acad_yr':acad_yr, 'sem':sem, 'category':category, 'code':arow[2], 'type':arow[3]}, 
-        {
-          $push: {
-            'schedule': {
-              'group': group,
-              'teaching_weeks':  teaching_weeks,
-              'day': arow[5],
-              'start_time': arow[6],
-              'end_time': arow[7],
-              'venue': venue,
-              'unscheduled_weeks': teaching_weeks
-            }
-          }
-        }, { new: true, upsert: true })
-        .then(function(result){
-          console.log(JSON.stringify(result));
-          // update venue utilization db 
-          VenueUtil.findOneAndUpdate({'venue': venue, 'acad_yr': acad_yr, 'sem': sem}, 
-            {
-              $push: {
-                'scheduled_time': {
-                  'course': arow[2],
-                  'courseType': arow[3],
-                  'group': group,
-                  'day': arow[5],
-                  'start_time': arow[6],
-                  'end_time': arow[7],
-                  'week': teaching_weeks,
-                }
-              }
-            },
-            {
-              new: true, upsert: true
-            }
-          ).then(function(updated_venue){
-            if(updated_venue == null){
-              VenueUtil.findOneAndUpdate({'venue': venue}, 
-              {
-                $push: {
-                  'acad_yr': acad_yr,
-                  'sem': sem,
-                  'scheduled_time': {
-                    'course': arow[2],
-                    'courseType': arow[3],
-                    'group': group,
-                    'day': arow[5],
-                    'start_time': arow[6],
-                    'end_time': arow[7],
-                    'week': teaching_weeks,
-                  }
-                }
-              }, 
-              {
-                new: true, upsert: true
-              }).then(function(new_venue){
-                console.log("Created new scheduled_time for venue\n" + JSON.stringify(new_venue));
-              }).catch(next);
-            } else {
-              console.log("Updated venue\n" + JSON.stringify(updated_venue));
-            }
-          }).catch(next);
-        }).catch(next);
+        const result_course = await process_course(arow, venue, group, teaching_weeks);
+        console.log(result_course);
+        const result_venue = await process_venue(arow, venue, group, teaching_weeks);
+        console.log(result_venue);
       }
     });
 }
